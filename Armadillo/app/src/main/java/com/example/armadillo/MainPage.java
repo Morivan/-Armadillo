b118ebb86360656cc40ccf967b2eaa1c02c36b16
Evvadium <playitcool44@gmail.com>
2024-03-06 16:25:54 +0300

    New features
    Added new activities and now button aren't useless

---
+++ Armadillo/app/src/main/java/com/example/armadillo/MainPage.java
@@ -0,0 +1,13 @@
+package com.example.armadillo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+    }
+}
