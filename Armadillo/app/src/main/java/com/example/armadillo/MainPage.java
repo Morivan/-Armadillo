0459766424e83c673046344fd87c0e7a22cfff2b
Evvadium <playitcool44@gmail.com>
2024-03-16 13:29:59 +0300

    New activities
    Added lectures activity and tests activity

--- Armadillo/app/src/main/java/com/example/armadillo/MainPage.java
+++ Armadillo/app/src/main/java/com/example/armadillo/MainPage.java
@@ -1,5 +1,8 @@
 package com.example.armadillo;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,32 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+
+        Button lecturesButton = findViewById(R.id.button_lectures);
+        Button tasksButton = findViewById(R.id.button_tasks);
+
+        lecturesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLecturesActivity();
+            }
+        });
+
+        tasksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openTasksActiviy();
+            }
+        });
+    }
+
+    private void openLecturesActivity(){
+        Intent intent = new Intent(MainPage.this, LecturesActivity.class);
+        startActivity(intent);
+    }
+
+    private void openTasksActiviy(){
+        Intent intent = new Intent(MainPage.this, TasksActivity.class);
+        startActivity(intent);
     }
 }
